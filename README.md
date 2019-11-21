e4aa4cdb983567ceba75a76865f6b147ae0c9c58
LeifChen <leifchen90@gmail.com>
2019-11-21 12:48:11 +0800

    feat: init project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hello-springcloud
+
 Learning Spring Cloud
---
+++ src/main/java/com/chen/springcloud/App.java
@@ -0,0 +1,19 @@
+package com.chen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用程序入口
+ * <p>
+ * @Author LeifChen
+ * @Date 2019/11/21
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}

cabea966558551ebd0ecb574d3ebe29979f60f6d
LeifChen <leifchen90@gmail.com>
2019-11-21 16:32:07 +0800

    feat: 更改模块名为hello-service

--- hello-service/src/main/java/com.chen.service/HelloServiceApp.java
+++ hello-service/src/main/java/com.chen.service/HelloServiceApp.java
@@ -2,18 +2,20 @@ package com.chen.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * Service应用程序入口
+ * HelloService应用程序入口
  * <p>
  * @Author LeifChen
  * @Date 2019/11/21
  */
 @SpringBootApplication
-public class ServiceApp {
+@EnableEurekaClient
+public class HelloServiceApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceApp.class, args);
+        SpringApplication.run(HelloServiceApp.class, args);
     }
 
 }

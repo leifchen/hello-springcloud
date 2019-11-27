c9d0722b1eaba42527f737311f125ee1b4ad14f4
LeifChen <leifchen90@gmail.com>
2019-11-27 10:07:59 +0800

    feat：新增注册中心Consul模块

---
+++ consul-producer/src/main/java/com/chen/ConsulProducerApp.java
@@ -0,0 +1,17 @@
+package com.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ConsulProducerApp应用程序入口
+ * <p>
+ * @Author LeifChen
+ * @Date 2019-11-26
+ */
+@SpringBootApplication
+public class ConsulProducerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulProducerApp.class, args);
+    }
+}
